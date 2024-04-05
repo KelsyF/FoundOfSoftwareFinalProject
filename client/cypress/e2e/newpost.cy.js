898595d9f3d0c9c4112688ac6789fef98ed54a1a
KelsyF <frank.ke@northeastern.edu>
2024-04-05 17:39:17 -0400

    'finished' first new post form test (there are holes)

--- client/cypress/e2e/newpost.cy.js
+++ client/cypress/e2e/newpost.cy.js
@@ -8,12 +8,21 @@ describe('New Post Form', () => {
     })
 
     it('Ask Question button creates and displays new post in All Questions', () => {
-
         cy.contains('Ask Question').click();
         cy.get('#formTitleInput').type('Test Question 1');
-        cy.get('#formTestInput').type('Test question 1 text.');
-        cy.get('#formTagInput').type('javascript test-1');
-        //How will we be implementing this considering our user profile setup?
+        cy.get('#formTextInput').type('Test question 1 text.');
+        cy.get('#formTagInput').type('javascript');
+
+        //How are we handling username now that we have a logged in profile?
         cy.get('#username')
+
+        cy.contains('Post').click();
+        cy.contains('Fake Stack Overflow');
+
+        //Need to add question titles once we've populated our db.
+        const qTitles = ['Test Question 1']
+        cy.get('.postTitle').each(($el, index, $list) => {
+            cy.wrap($el).should('contain', qTitles[index]);
+        });
     })
 })
